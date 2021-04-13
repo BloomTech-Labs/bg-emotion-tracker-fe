4a117e69d2b8534e8347ccd196cce5e07e5c1e7c
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-04-13 12:18:39 -0400

    adding download button and logic for QRgenerator to download QR code in png format, changing qrcode value and format

--- src/utils/qrGenerator.js
+++ src/utils/qrGenerator.js
@@ -2,26 +2,43 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 
 const QRGenerator = props => {
-  let valueString = 'http://picturesofpeoplescanningqrcodes.tumblr.com/';
+  let valueString = 'https://www.youtube.com/';
+
+  const downloadQR = () => {
+    const canvas = document.getElementById('qrcode-1');
+    const pngUrl = canvas
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream');
+    let downloadLink = document.createElement('a');
+    downloadLink.href = pngUrl;
+    downloadLink.download = 'qrcode-1.png';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
 
   return (
-    <QRCode
-      value={valueString}
-      size={128}
-      bgColor={'#ffffff'}
-      fgColor={'#000000'}
-      level={'L'}
-      includeMargin={false}
-      renderAs={'svg'}
-      imageSettings={{
+    <div>
+      <QRCode
+        id="qrcode-1"
+        value={valueString}
+        size={290}
+        //bgColor={'#ffffff'}
+        //fgColor={'#000000'}
+        level={'H'}
+        includeMargin={true}
+        //renderAs={'svg'}
+        /*imageSettings={{
         src: 'https://static.zpao.com/favicon.png',
         x: null,
         y: null,
         height: 24,
         width: 24,
         excavate: true,
-      }}
-    />
+      }}*/
+      />
+      <button onClick={downloadQR}> Download QR</button>
+    </div>
   );
 };
 
