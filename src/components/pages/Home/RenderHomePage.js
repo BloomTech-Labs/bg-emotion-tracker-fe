f56e9c8e9806b66f75dd9b59f602ee8141c06f80
Sebastian <sebastianmendoza482@gmail.com>
2021-04-15 11:14:37 -0700

    removed sampleData and moved navbar into separate component

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,111 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import {
-  Statistic,
-  Card,
-  Row,
-  Col,
-  Avatar,
-  Menu,
-  Dropdown,
-  Button,
-} from 'antd';
-import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
-import { DownOutlined, LikeOutlined, UserOutlined } from '@ant-design/icons';
+import NavBar from '../../common/NavBar';
+import { Card } from 'antd';
 
 const StyledHomePage = styled.header`
   display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  background: royalblue;
-  text-align: center;
-  .avatar {
-    margin: 2%;
-  }
-  h1 {
-    margin: 1% 30% 0%;
-  }
-  button {
-    margin: 2%;
-  }
+  margin: 10%;
+  justify-content: center;
 `;
-
-const StyledData = styled.div`
-  padding: 5%;
-  margin: 2%;
-`;
-const menu = (
-  <Menu>
-    <Menu.Item key="1" icon={<UserOutlined />}>
-      Add User
-    </Menu.Item>
-    <Menu.Item key="2" icon={<UserOutlined />}>
-      Generate QR
-    </Menu.Item>
-    <Menu.Item key="3" icon={<UserOutlined />}>
-      Print ID
-    </Menu.Item>
-  </Menu>
-);
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
     <>
+      <NavBar titleName="Dashboard" backgroundColor="#293845" />
       <StyledHomePage>
-        <Avatar size="large" icon={<UserOutlined />} className="avatar" />
-        <h1>Hi {userInfo.name}, Welcome.</h1>
-        <p></p>
-        <Dropdown overlay={menu}>
-          {/* <Button> */}
-          <DownOutlined />
-          {/* </Button> */}
-        </Dropdown>
+        <Card>Sample Data</Card>
       </StyledHomePage>
-      <StyledData>
-        <div className="site-statistic-demo-card">
-          <Row gutter={16}>
-            <Col span={12}>
-              <Card>
-                <Statistic
-                  title="Happiness Rate"
-                  value={11.28}
-                  precision={2}
-                  valueStyle={{ color: '#3f8600' }}
-                  prefix={<ArrowUpOutlined />}
-                  suffix="%"
-                />
-              </Card>
-            </Col>
-            <Col span={12}>
-              <Card>
-                <Statistic
-                  title="Attendance This Week"
-                  value={9.3}
-                  precision={2}
-                  valueStyle={{ color: '#cf1322' }}
-                  prefix={<ArrowDownOutlined />}
-                  suffix="%"
-                />
-              </Card>
-            </Col>
-          </Row>
-        </div>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Statistic
-              title="Thumbs Today"
-              value={1128}
-              prefix={<LikeOutlined />}
-            />
-          </Col>
-          <Col span={12}>
-            <Statistic title="Kids Logged" value={93} suffix="/ 100" />
-          </Col>
-        </Row>
-        <Button> import CSV </Button>
-      </StyledData>
     </>
   );
 }
