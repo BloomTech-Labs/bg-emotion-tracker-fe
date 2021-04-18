0d98e626e6360758f299eab9aa9204c8ff705f2e
sirbaconsalot <donavynhaley@gmail.com>
2021-04-18 15:03:48 -0700

    Added upload feature, after uploading csv data gets displayed to user in list

--- src/components/pages/Import/ImportModal.js
+++ src/components/pages/Import/ImportModal.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button } from 'antd';
 import { parseCSV } from '../../../utils/parseCSV';
+
+import ImportUpload from './ImportUpload';
 // Might have to check if file is type csv
 // Import for kids csv
 const ImportModal = () => {
@@ -21,19 +23,21 @@ const ImportModal = () => {
   return (
     <>
       <Button type="primary" onClick={showModal}>
-        Import
+        Add
       </Button>
       <Modal
-        title="Import Kids"
+        title="Import Member"
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
+        width={'60%'}
       >
         <div>
-          <h3>Single Entry</h3>
+          <h3>Add Individual Member</h3>
         </div>
         <div>
           <h3>Upload</h3>
+          <ImportUpload />
         </div>
       </Modal>
     </>
--- src/components/pages/Import/ImportUpload.js
+++ src/components/pages/Import/ImportUpload.js
@@ -1,2 +1,36 @@
-import React from 'react';
-const ImportUpload = () => {};
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
+import MemberList from './MemberList';
+const ImportUpload = props => {
+  const [fileState, setFileState] = useState([]);
+  const onFormSubmit = () => {
+    // upload data to server
+    return;
+  };
+
+  const onChange = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = readerEvent => {
+      const members = readerEvent.target.result;
+      const listOfMembers = members.match(/.{1,10}/g); // Splits input every 10 characters
+      setFileState(...fileState, listOfMembers);
+      console.warn(fileState);
+    };
+  };
+
+  useEffect(() => {}, [fileState]);
+
+  return (
+    <>
+      <MemberList listOfMembers={fileState} />
+      <div onSubmit={onFormSubmit}>
+        <input type="file" name="fileUplaod" onChange={e => onChange(e)} />
+        <Button type="primary">Submit</Button>
+      </div>
+    </>
+  );
+};
+
+export default ImportUpload;
