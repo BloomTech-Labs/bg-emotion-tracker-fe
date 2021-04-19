d488e94763895281b42c80b8ae0ab570effd3d30
Sebastian <sebastianmendoza482@gmail.com>
2021-04-19 15:23:06 -0700

    donny v2 commit

---
+++ src/components/pages/Members/ViewMembers.js
@@ -0,0 +1,59 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import NavBar from '../../common/NavBar';
+import { Card, Button, Table, Menu, Dropdown } from 'antd';
+import { Link } from 'react-router-dom';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
+const StyledMembers = styled.header`
+  margin: 10%;
+  justify-content: center;
+  button {
+  }
+  h2,
+  button {
+    display: flex;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+`;
+
+const menu = (
+  <Menu>
+    <Link to="/">
+      <Menu.Item key="1" icon={<UserOutlined />}>
+        Home
+      </Menu.Item>
+    </Link>
+    <Link to="manage-members">
+      <Menu.Item key="2" icon={<UserOutlined />}>
+        Manage Members
+      </Menu.Item>
+    </Link>
+    <Link to="manage-programs">
+      <Menu.Item key="3" icon={<UserOutlined />}>
+        Manage Programs
+      </Menu.Item>
+    </Link>
+    <Link to="manage-staff">
+      <Menu.Item key="4" icon={<UserOutlined />}>
+        Manage Staff
+      </Menu.Item>
+    </Link>
+  </Menu>
+);
+
+function ViewMembers(props) {
+  return (
+    <>
+      <NavBar titleName="View Members" backgroundColor="green" />
+      <StyledMembers>
+        <h2>Name Placeholder</h2>
+        <Button> Add </Button>
+        <Table>Sample Data</Table>
+      </StyledMembers>
+    </>
+  );
+}
+export default ViewMembers;
