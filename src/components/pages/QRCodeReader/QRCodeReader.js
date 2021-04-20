7a5aa98c95b6d416857f0140e6085d8efaf0fb7e
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-04-20 16:52:18 -0400

    creating component for QRCodeReader

---
+++ src/components/pages/QRCodeReader/QRCodeReader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import QrReader from 'modern-react-qr-reader';
+
+const QRCodeReader = props => {
+  const handleError = err => {
+    console.error(err);
+  };
+
+  const handleScan = data => {
+    if (data) {
+      console.log(data);
+    }
+  };
+
+  return (
+    <div>
+      <QrReader
+        delay={300}
+        facingMode={'environment'}
+        style={{ width: '100%' }}
+        onError={handleError}
+        onScan={handleScan}
+      />
+    </div>
+  );
+};
+
+export default QRCodeReader;
