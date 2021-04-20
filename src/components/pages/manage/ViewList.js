af8dfd329aa5ac32ef13adf1e894ee81a0e9462f
Leah Tabush <leahtabush@gmail.com>
2021-04-19 21:50:44 -0700

    added some styles so the nav bar and members page look good on wider width screens also

--- src/components/pages/manage/ViewList.js
+++ src/components/pages/manage/ViewList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { PageHeader, Table } from 'antd';
+import styled from 'styled-components';
+
+const StyledList = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 export default function ViewList(props) {
   const { title, sortedBy, columns, members } = props;
 
   return (
-    <div>
+    <StyledList>
       <PageHeader
         className="site-page-header"
         title={title}
@@ -17,6 +23,6 @@ export default function ViewList(props) {
         style={{ paddingLeft: 8 }}
         pagination={{ position: ['none', 'bottomRight'] }}
       />
-    </div>
+    </StyledList>
   );
 }
