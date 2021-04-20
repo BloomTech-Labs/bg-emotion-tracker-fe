38ae88af6246b3ce316fedca184723d2b8afc6a1
Sebastian <sebastianmendoza482@gmail.com>
2021-04-20 09:10:31 -0700

    list rendering added

--- src/components/common/ViewSingleton/ViewList.js
+++ src/components/common/ViewSingleton/ViewList.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import ViewCollapse from './ViewCollapse';
+import { List } from 'antd';
+import { propTypes } from 'qrcode.react';
 
-function ViewList() {
-  return <div></div>;
+const data = [
+  {
+    id: '121e21ex12',
+    Name: 'Dungeons and Dragons',
+    Role: '',
+    Desc: 'Members get together to play the tabletop game D&D',
+    Location: 'adsndjasn12', // Id of club,
+  },
+  {
+    id: '121e21ex12',
+    Name: 'Dungeons and Dragons',
+    Role: '',
+    Desc: 'Members get together to play the tabletop game D&D',
+    Location: 'adsndjasn12', // Id of club,
+  },
+];
+function ViewList(props) {
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta title={item.Name} />
+        </List.Item>
+      )}
+    />
+  );
 }
 
 export default ViewList;
