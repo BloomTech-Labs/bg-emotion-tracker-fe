379b8094f3265ddeb4678d5174e3631d99d7540e
Sebastian <sebastianmendoza482@gmail.com>
2021-04-20 19:26:44 -0700

    Added routes to Leah's navbar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -4,6 +4,9 @@ import { Menu, Dropdown, Button } from 'antd';
 import {
   MenuOutlined,
   UserOutlined,
+  TeamOutlined,
+  LineChartOutlined,
+  CalendarOutlined,
 } from '@ant-design/icons';
 import logo from '../../assets/images/BGC-logo-header.png';
 import { useHistory } from 'react-router';
@@ -17,14 +20,14 @@ const StyledNavBar = styled.header`
   text-align: center;
   img,
   .menu-container {
-    margin: 1rem;
+    margin: 2%;
   }
   .menu-container {
     text-align: left;
     width: 109px;
   }
   img {
-    height: 5rem;
+    height: 60px;
     width: auto;
   }
   h1 {
@@ -33,12 +36,19 @@ const StyledNavBar = styled.header`
   }
 `;
 
-const menu = (
-  <Menu>
-    <Link to="/">
-      <Menu.Item key="1" icon={<UserOutlined />}>
+function NavBar(props) {
+  const { userInfo, authService } = props;
+
+  const menu = (
+    <Menu>
+      <Menu.Item
+        key="1"
+        icon={<LineChartOutlined />}
+        onClick={() => history.push('/')}
+      >
         Home
       </Menu.Item>
+
       <Menu.Item
         key="2"
         icon={<UserOutlined />}
@@ -46,26 +56,25 @@ const menu = (
       >
         Manage Members
       </Menu.Item>
+
       <Menu.Item
         key="3"
         icon={<CalendarOutlined />}
-        onClick={() => history.push('manage-programs')}
+        onClick={() => history.push('/manage-programs')}
       >
         Manage Programs
       </Menu.Item>
+
       <Menu.Item
         key="4"
-        onClick={() => history.push('/manage-staff')}
         icon={<TeamOutlined />}
+        onClick={() => history.push('/manage-staff')}
       >
         Manage Staff
       </Menu.Item>
-    </Link>
-  </Menu>
-);
-
-function NavBar(props) {
-  const { userInfo, authService } = props;
+    </Menu>
+  );
+  const history = useHistory();
   return (
     <>
       <StyledNavBar backgroundColor={props.backgroundColor}>
