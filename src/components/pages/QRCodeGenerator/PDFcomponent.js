03fbf2ba13905141b9d6da496363e1cbe1b8be13
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-04-24 02:18:54 -0400

    adding StyleSheet feature

--- src/components/pages/QRCodeGenerator/PDFcomponent.js
+++ src/components/pages/QRCodeGenerator/PDFcomponent.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Document, Page, Text, Image, View } from '@react-pdf/renderer';
+import {
+  Document,
+  Page,
+  Text,
+  Image,
+  View,
+  StyleSheet,
+} from '@react-pdf/renderer';
 
 const PDFcomponent = props => {
   const { PDFImageIds } = props;
 
   const IdsArray = [];
+
+  const styles = StyleSheet.create({
+    page: {
+      backgroundColor: 'white',
+      flexDirection: 'row',
+    },
+    view: {
+      display: 'inline',
+    },
+    image: {
+      width: '50%',
+    },
+    text: {
+      color: '#0081C6',
+    },
+  });
 
   const resultArray = PDFImageIds.map(id => {
     IdsArray.push(id);
@@ -15,18 +38,10 @@ const PDFcomponent = props => {
     <Document>
       {resultArray.map((dataURL, id) => {
         return (
-          <Page
-            key={`PageId_${id}`}
-            size={'A10'}
-            style={{ backgroundColor: 'white' }}
-          >
-            <View style={{ display: 'inline' }}>
-              <Image
-                allowDangerousPaths
-                src={dataURL}
-                style={{ width: '50%' }}
-              />
-              <Text style={{ color: '#0081C6' }}>{IdsArray[id]}</Text>
+          <Page key={`PageId_${id}`} size={'A10'} style={styles.page}>
+            <View style={styles.view}>
+              <Image allowDangerousPaths src={dataURL} style={styles.image} />
+              <Text style={styles.text}>{IdsArray[id]}</Text>
             </View>
           </Page>
         );
