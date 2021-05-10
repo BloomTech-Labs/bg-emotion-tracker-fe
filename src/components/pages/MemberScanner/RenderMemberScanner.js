e26608598c670a9ebe973f57c1e4c342e16ff98d
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-05-10 13:04:15 -0400

    Some untracked changes

---
+++ src/components/pages/MemberScanner/RenderMemberScanner.js
@@ -0,0 +1,35 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import NavBar from '../../common/NavBar';
+import { QRCodeReader } from '../QRCodeReader';
+
+const StyledMemberScanner = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 1200px;
+  max-width: 90%;
+  margin: 3rem auto;
+`;
+
+function RenderMemberScanner(props) {
+  const { userInfo /*authService*/ } = props;
+
+  const handleScan = data => {
+    if (data) {
+      console.log(data);
+    }
+  };
+
+  return (
+    <>
+      <NavBar titleName="Dashboard" backgroundColor="#293845" />
+      <StyledMemberScanner>
+        <h2>Scaner</h2>
+        <QRCodeReader handleScan={handleScan} />
+      </StyledMemberScanner>
+    </>
+  );
+}
+export default RenderMemberScanner;
