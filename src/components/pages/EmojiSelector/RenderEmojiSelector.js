943a8fc6b54dbfde86de6d50758e641ae6f3b730
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-05-12 17:39:13 -0400

    Adding memberFeedback hook

--- src/components/pages/EmojiSelector/RenderEmojiSelector.js
+++ src/components/pages/EmojiSelector/RenderEmojiSelector.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import NavBar from '../../common/NavBar';
 
@@ -11,14 +11,21 @@ const StyledEmojiSelector = styled.header`
   margin: 3rem auto;
 `;
 
+const initialMemberFeedback = {
+  memeberId: 'None',
+  emojiCode: 'None',
+};
+
 function RenderEmojiSelector(props) {
   const locationState = props.pageProps.history.location.state;
+
+  const [memberFeedback, setMemberFeedback] = useState(initialMemberFeedback);
 
   return (
     <>
       <NavBar titleName="Dashboard" backgroundColor="#293845" />
       <StyledEmojiSelector>
-        <h2>Emoji Selector</h2>
+        <h2>Rate the Event</h2>
         {locationState ? <p>Choose emoji</p> : <p>Use scanner first</p>}
       </StyledEmojiSelector>
     </>
