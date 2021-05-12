95edba5a19ccaf4673858e45b9a9940fc54480be
pak11273 <pak11273@gmail.com>
2021-05-12 10:09:59 -0500

    adds authstate to homecontainer so we can use user tokens for okta

--- src/components/pages/Home/HomeContainer.js
+++ src/components/pages/Home/HomeContainer.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 
 import RenderHomePage from './RenderHomePage';
 
 function HomeContainer({ LoadingComponent }) {
-  const { authService } = useOktaAuth();
+  const { authState, authService } = useOktaAuth();
   const [userInfo] = useState(null);
+  console.log('auth: ', authState);
+  // we get access token and id token from authState
+  // which one do we use to make backend requests?
+  useEffect(() => {
+    // make a request to backend for current user and put role into context api
+  });
 
   return (
     <>
