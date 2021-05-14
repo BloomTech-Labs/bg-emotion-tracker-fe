4841985a8c1942fd94d774ab3cd001c07a273528
sirbaconsalot <donavynhaley@gmail.com>
2021-05-13 21:55:30 -0700

    Created admin dashboard and club director dashboard pages. Hooked new dashboard pages into redirect. Each login successfully brings user to correct dashboard.

--- src/components/pages/Home/HomeContainer.js
+++ src/components/pages/Home/HomeContainer.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import { UserContext } from '../../../state/contexts';
-import axios from 'axios';
 
 import RenderHomePage from './RenderHomePage';
 import { getUserProfile } from '../../../state/actions';
 import { useHistory } from 'react-router-dom';
 
 const HomeContainer = props => {
-  const [userInfo] = useState(null);
   const { authState } = useOktaAuth();
   const context = useContext(UserContext);
   const { push } = useHistory();
@@ -20,13 +18,17 @@ const HomeContainer = props => {
   let role = context.user.roles && context.user.roles[0].role.name;
 
   // initial redirect to correct user dashboard
-  if (role === 'YDP') {
+  if (role === 'ADMIN') {
+    push('./AdminDashboard');
+  } else if (role === 'CD') {
+    push('./ClubDirectorDashboard');
+  } else if (role === 'YDP') {
     push('/YDPDashboard');
   }
 
   return (
     <>
-      <RenderHomePage userInfo={userInfo} />
+      <RenderHomePage />
     </>
   );
 };
