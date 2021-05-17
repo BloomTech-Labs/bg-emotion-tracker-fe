854c92f121c5a5ff8401ba7b910451126b1f5051
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-05-17 12:09:11 -0400

    Removing redundant code from EmojiSelectCheck

--- src/components/pages/EmojiSelectCheck/RenderEmojiSelectCheck.js
+++ src/components/pages/EmojiSelectCheck/RenderEmojiSelectCheck.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-//import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import styled from 'styled-components';
 import NavBar from '../../common/NavBar';
-import { Card, Button, Row, Col, Divider, Radio } from 'antd';
+import { Button, Divider, Radio } from 'antd';
 import axios from 'axios';
 import { baseUrl } from '../../../api/index';
 
@@ -16,8 +15,6 @@ const StyledEmojiSelectCheck = styled.header`
   margin: 3rem auto;
 `;
 
-//const style = { background: '#ffffff', padding: '2px 0' };
-
 const options = [
   { label: '😁', value: '1F601' },
   { label: '🙂', value: '1F642' },
@@ -25,18 +22,6 @@ const options = [
   { label: '🙁', value: '1F641' },
   { label: '😞', value: '1F61E' },
 ];
-
-// const style= styled.div`
-// font-size:15rem;
-// background: '#ffffff';
-// padding: '8px 0';
-// `;
-
-const StyledEmojis = styled.div`
-  font-size: 3rem;
-  background: '#ffffff';
-  padding: '2px 0';
-`;
 
 const StyledButton = styled(Button)`
   background-color: 293845;
@@ -98,31 +83,6 @@ function RenderEmojiSelectCheck(props) {
       <NavBar titleName="Dashboard" backgroundColor="#293845" />
       <StyledEmojiSelectCheck>
         <h2>Select Emoji</h2>
-        {/*
-        <StyledEmojis>
-          <>
-            <Divider orientation="left">***</Divider>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-              <Col className="gutter-row" span={6}>
-                <div style={style} >😁</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                <div style={style}>🙂</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                <div style={style}>😐</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                <div style={style}>🙁</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                <div style={style}>😞</div>
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          </>
-        </StyledEmojis>
-        */}
         <Divider orientation="left">***</Divider>
         <Radio.Group
           options={options}
