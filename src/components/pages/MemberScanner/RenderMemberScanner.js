1bac3dae48b5fc42f3d824d5289eca02ef3330ee
sirbaconsalot <donavynhaley@gmail.com>
2021-05-27 15:03:52 -0700

    working on fix for styling bug

--- src/components/pages/MemberScanner/RenderMemberScanner.js
+++ src/components/pages/MemberScanner/RenderMemberScanner.js
@@ -16,6 +16,7 @@ const StyledMemberScanner = styled.header`
   display: flex;
   margin-left: 30%;
   margin-right: 30%;
+  margin-top: 15%;
   flex-direction: column;
   text-align: center;
   @media (min-width: 1240px) {
