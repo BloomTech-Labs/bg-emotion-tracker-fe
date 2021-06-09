199c5515bbf94bb90c62dc84a7f0125f95727d9e
L-Steinmacher <dasai.sacul@gmail.com>
2021-06-09 08:49:44 -0700

    removes all items from local storage upon logout

--- src/components/common/Logout.js
+++ src/components/common/Logout.js
@@ -7,6 +7,7 @@ export const Logout = () => {
   userCtx.resetUser();
   localStorage.removeItem('okta-token-storage');
   localStorage.removeItem('okta-cache-storage');
-
+  localStorage.removeItem('role');
+  localStorage.removeItem('okta-pkce-storage');
   return <Redirect to="/login">Logout</Redirect>;
 };
