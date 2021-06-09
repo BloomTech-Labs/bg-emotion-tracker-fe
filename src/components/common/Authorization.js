95eea4166f0664b3ea5d4d5b3fe691cee311496f
L-Steinmacher <dasai.sacul@gmail.com>
2021-06-08 18:46:54 -0700

    fixes bug where reloading a page causes an error

--- src/components/common/Authorization.js
+++ src/components/common/Authorization.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { UserContext } from '../../state/contexts';
-
 export const Authorization = (roles, WrappedComponent) => {
   let { user } = useContext(UserContext);
   let role = user.roles && user.roles[0].role.name;
   let history = useHistory();
+  if (role) {
+    localStorage.setItem('role', role);
+  }
   useEffect(() => {
-    if (!role && !roles.includes(role)) {
+    let localRole = localStorage.getItem('role');
+    if (localRole && !roles.includes(localRole)) {
       history.push('/unauthorized');
     }
   }, [role]);
