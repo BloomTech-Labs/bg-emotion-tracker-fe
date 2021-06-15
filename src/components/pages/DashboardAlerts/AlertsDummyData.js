5e193970ab6a89f75583f76e51859c0cfd53f26c
L-Steinmacher <dasai.sacul@gmail.com>
2021-06-14 20:11:15 -0700

    the badges update with data live

--- src/components/pages/DashboardAlerts/AlertsDummyData.js
+++ src/components/pages/DashboardAlerts/AlertsDummyData.js
@@ -2,94 +2,196 @@
  * This dummy datais to represent the json data that will be coming from the back end about member
  * who reported a negitive sentament.
  */
-
 export const dummyData = [
   {
-    clubid: 1,
-    clubname: 'Jackson',
-    clubmembers: [
-      // for each member there is an alert
-      {
-        memberid: 1,
-        reactions: [
-          {
-            clubid: 1,
-            activity: {
-              activityid: 1,
-              activityname: 'bowling',
-            },
-            reactionvalue: '10D3',
-            intigerValue: 1,
-            resolved: false, //need to set local state of bool as well as on the backend
+    reactionresolved: false,
+    member: {
+      member_table_id: 25,
+      memberid: 'testmember2',
+      clubs: [
+        {
+          club: {
+            clubid: 18,
+            clubname: 'Johnston',
           },
-        ],
+        },
+        {
+          club: {
+            clubid: 19,
+            clubname: 'Marley',
+          },
+        },
+        {
+          club: {
+            clubid: 20,
+            clubname: 'Morton',
+          },
+        },
+      ],
+    },
+    reaction: {
+      reactionid: 37,
+      reactionvalue: '1F62D',
+      reactionint: 1,
+    },
+    clubactivity: {
+      club: {
+        clubid: 20,
+        clubname: 'Morton',
       },
-    ],
+      activity: {
+        activityid: 14,
+        activityname: 'Club Checkout',
+      },
+    },
+    createddate: '2021-06-14 15:59:49',
   },
   {
-    clubid: 2,
-    clubname: 'Anderson',
-    clubmembers: [
-      // for each member there is an alert
-      {
-        memberid: 3,
-        reactions: [
-          {
-            clubid: 2,
-            activity: {
-              activityid: 1,
-              activityname: 'bowling',
-            },
-            reactionvalue: '10D3',
-            intigerValue: 1,
-            resolved: false, //need to set local state of bool as well as on hte backend
+    reactionresolved: false,
+    member: {
+      member_table_id: 27,
+      memberid: 'testmember4',
+      clubs: [
+        {
+          club: {
+            clubid: 15,
+            clubname: 'Anderson',
           },
-        ],
+        },
+      ],
+    },
+    reaction: {
+      reactionid: 37,
+      reactionvalue: '1F62D',
+      reactionint: 1,
+    },
+    clubactivity: {
+      club: {
+        clubid: 15,
+        clubname: 'Anderson',
       },
-    ],
+      activity: {
+        activityid: 7,
+        activityname: 'Club Checkin',
+      },
+    },
+    createddate: '2021-06-14 15:59:49',
   },
   {
-    clubid: 2,
-    clubname: 'Anderson',
-    clubmembers: [
-      // for each member there is an alert
-      {
-        memberid: 2,
-        reactions: [
-          {
-            clubid: 2,
-            activity: {
-              activityid: 1,
-              activityname: 'bowling',
-            },
-            reactionvalue: '10D3',
-            intigerValue: 1,
-            resolved: false, //need to set local state of bool as well as on hte backend
+    reactionresolved: false,
+    member: {
+      member_table_id: 25,
+      memberid: 'testmember2',
+      clubs: [
+        {
+          club: {
+            clubid: 18,
+            clubname: 'Johnston',
           },
-        ],
+        },
+        {
+          club: {
+            clubid: 19,
+            clubname: 'Marley',
+          },
+        },
+        {
+          club: {
+            clubid: 20,
+            clubname: 'Morton',
+          },
+        },
+      ],
+    },
+    reaction: {
+      reactionid: 32,
+      reactionvalue: '1F61E',
+      reactionint: 1,
+    },
+    clubactivity: {
+      club: {
+        clubid: 20,
+        clubname: 'Morton',
       },
-    ],
+      activity: {
+        activityid: 14,
+        activityname: 'Club Checkout',
+      },
+    },
+    createddate: '2021-06-14 15:59:49',
   },
   {
-    clubid: 2,
-    clubname: 'Anderson',
-    clubmembers: [
-      // for each member there is an alert
-      {
-        memberid: 2,
-        reactions: [
-          {
-            clubid: 2,
-            activity: {
-              activityid: 2,
-              activityname: 'skydiving',
-            },
-            reactionvalue: '10D3',
-            intigerValue: 1,
-            resolved: false, //need to set local state of bool as well as on hte backend
+    reactionresolved: false,
+    member: {
+      member_table_id: 27,
+      memberid: 'testmember4',
+      clubs: [
+        {
+          club: {
+            clubid: 15,
+            clubname: 'Anderson',
           },
-        ],
+        },
+      ],
+    },
+    reaction: {
+      reactionid: 32,
+      reactionvalue: '1F61E',
+      reactionint: 1,
+    },
+    clubactivity: {
+      club: {
+        clubid: 15,
+        clubname: 'Anderson',
       },
-    ],
+      activity: {
+        activityid: 7,
+        activityname: 'Club Checkin',
+      },
+    },
+    createddate: '2021-06-14 15:59:49',
+  },
+  {
+    reactionresolved: false,
+    member: {
+      member_table_id: 24,
+      memberid: 'testmember1',
+      clubs: [
+        {
+          club: {
+            clubid: 15,
+            clubname: 'Anderson',
+          },
+        },
+        {
+          club: {
+            clubid: 16,
+            clubname: 'Caitlin',
+          },
+        },
+        {
+          club: {
+            clubid: 17,
+            clubname: 'Grossman',
+          },
+        },
+      ],
+    },
+    reaction: {
+      reactionid: 32,
+      reactionvalue: '1F61E',
+      reactionint: 1,
+    },
+    clubactivity: {
+      club: {
+        clubid: 16,
+        clubname: 'Caitlin',
+      },
+      activity: {
+        activityid: 14,
+        activityname: 'Club Checkout',
+      },
+    },
+    createddate: '2021-06-14 15:59:49',
   },
 ];
--- src/components/pages/DashboardAlerts/RenderDashboardAlerts.js
+++ src/components/pages/DashboardAlerts/RenderDashboardAlerts.js
@@ -1,40 +1,79 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './DashboardAlerts.css';
-import { Badge } from 'antd';
+import { Badge, Menu } from 'antd';
 import { dummyData } from './AlertsDummyData';
+import { AdminContext } from '../../../state/contexts';
+import { getClubs } from '../../../state/actions';
+import axios from 'axios';
+import MenuItem from 'antd/lib/menu/MenuItem';
 
 function RenderDashboardAlerts() {
+  const context = useContext(AdminContext);
+  let [reactions, setReactions] = useState([]);
+
   /*
    * This function combines the locations into one object with the value of the
    * number of times the location appears. Main pourpous is to not have repeating locations and have a number coralating
    * to the number of alerts.
    */
-  const combineFlags = dummyData => {
+  function combine_flags(arr) {
     const rtn = {};
-    dummyData.forEach(location => {
-      if (!(location.clubname in rtn)) {
-        rtn[location.clubname] = 0;
+
+    arr.forEach(alert => {
+      if (!(alert.clubactivity.club.clubname in rtn)) {
+        rtn[alert.clubactivity.club.clubname] = 0;
       }
-      rtn[location.clubname] += 1;
+      rtn[alert.clubactivity.club.clubname] += 1;
     });
+    console.log(rtn);
     return rtn;
+  }
+
+  const fetchClubs = () => {
+    getClubs('authState', context);
   };
 
-  let flagObj = combineFlags(dummyData);
+  useEffect(() => {
+    fetchClubs();
+  }, []);
+
+  // Get all membersreactions
+  const getMembersReaction = () => {
+    let tokenObj = JSON.parse(localStorage.getItem('okta-token-storage'));
+
+    const promise = axios.get(
+      `https://bg-emotion-tracker-be-b.herokuapp.com/memberreactions/alert`,
+      {
+        headers: {
+          Authorization: `Bearer ${tokenObj.accessToken.accessToken}`,
+        },
+      }
+    );
+
+    const dataPromise = promise.then(res => {
+      setReactions(res.data);
+      return res.data;
+    });
+    return dataPromise;
+  };
+
+  useEffect(() => {
+    getMembersReaction();
+  }, []);
+  let flagObj = combine_flags(reactions);
 
   return (
     <>
-      <div className="alerts container">
-        <h2 className="alerts header"> Alerts</h2>
-        {Object.keys(flagObj).map(location => (
-          <div className="alert div" key={location}>
-            <h3>{location}</h3>
-            <Badge count={flagObj[location]} className="badge">
-              <a href="#" className="alertLink" />
+      <Menu>
+        {context.clubs.map(club => (
+          <div className="club alert" key={club.clubid}>
+            <h3 className="mi">{club.clubname}</h3>
+            <Badge count={flagObj[club.clubname]} className="badge">
+              <a href="/alerts" className="alertLink" />
             </Badge>
           </div>
         ))}
-      </div>
+      </Menu>
     </>
   );
 }
