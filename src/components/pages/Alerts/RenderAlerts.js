cbf77b20c670a97b23958684943bedf2d519d0b0
ashtilawat23 <ashtilawat23@gmail.com>
2021-06-18 11:43:43 -0400

    Context wired up for Leaderboard and Alerts with CSS fixes

--- src/components/pages/Alerts/RenderAlerts.js
+++ src/components/pages/Alerts/RenderAlerts.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { LayoutContainer } from '../../common/';
 import NavBar from '../../common/NavBar';
 import Tabs from '../../common/Tabs';
 import './Alerts.css';
 import { Layout } from 'antd';
 import NavMenu from '../../common/NavMenu';
+import { AdminContext } from '../../../state/contexts';
+import { getClubs, getMembersReaction } from '../../../state/actions';
 const { Content, Sider } = Layout;
 
-const dummyClubData = [
-  'Anderson',
-  'Caitlin',
-  'Grossman',
-  'Johnston',
-  'Marley',
-  'Morton',
-  'Notter',
-  'Stelle',
-  'Jefferson',
-];
-
 function RenderAlerts(props) {
+  const context = useContext(AdminContext);
+
+  useEffect(() => {
+    if (context.clubs.length === 0) {
+      getClubs('authState', context);
+    }
+    if (context.memberReactions === 0) {
+      getMembersReaction('authState', context);
+    }
+  }, []);
+
+  console.log(context.memberReactions);
+
   return (
     <LayoutContainer>
       <NavBar titleName={'Alerts'} backgroundColor="#293845" />
@@ -28,19 +31,19 @@ function RenderAlerts(props) {
           <NavMenu />
         </Sider>
         <Content>
-            <Tabs>
-              {dummyClubData.map(elem => (
-                <div label={elem}>
-                  <div className="under-tabs-container">
-                    <div className="flags box">
-                      <h2>Negative Sentiment</h2>
-                    </div>
-                    <div className="insights box">
-                      <h2>Insights</h2>
-                    </div>
+          <Tabs>
+            {context.clubs.map(club => (
+              <div label={club.clubname}>
+                <div className="under-tabs-container" key={club.clubid}>
+                  <div className="flags box">
+                    <h2>Negative Sentiment</h2>
+                  </div>
+                  <div className="insights box">
+                    <h2>Insights</h2>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))}
           </Tabs>
         </Content>
       </Layout>
