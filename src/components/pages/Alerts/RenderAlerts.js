dc7c8c8973e4a79795b088afb7b8fa6e0f85ba89
shawn2776 <shawn.harrington2776@gmail.com>
2021-07-21 13:14:24 -0700

    Created a card grid system for each club. Each alert card displays emoji and activity. On hover over the card, a tooltip pops up with additional information, including memberid, activity, time, elapsed time, and emoji. Still need to configure elapsed time properly, Change the color of the card and tooltip depending on elapsed time, and align the cards so they don't wrap or go off the card when there are more than about 6 cards

--- src/components/pages/Alerts/RenderAlerts.js
+++ src/components/pages/Alerts/RenderAlerts.js
@@ -3,7 +3,7 @@ import { LayoutContainer } from '../../common/';
 import NavBar from '../../common/NavBar';
 import Tabs from '../../common/Tabs';
 import './Alerts.css';
-import { Layout, Button } from 'antd';
+import { Layout, Button, Card, Tooltip } from 'antd';
 import NavMenu from '../../common/NavMenu';
 import { AdminContext } from '../../../state/contexts';
 import { getClubs, getMembersReaction } from '../../../state/actions';
@@ -12,6 +12,26 @@ import '../../../styles/styles.css';
 import axios from 'axios';
 
 const { Content, Sider } = Layout;
+
+const gridStyle1 = {
+  width: '10%',
+  textAlign: 'center',
+  fontWeight: 'bold',
+};
+
+const gridStyle2 = {
+  width: '15%',
+  textAlign: 'center',
+};
+
+function ElapsedTime(createddate) {
+  // let newCreatedDate = createddate.getHours() + ':' + createddate.getMinutes();
+  let today = new Date();
+  let time = today.getHours() + ':' + today.getMinutes();
+  console.log(`CreatedDate: ${typeof createddate}`);
+  console.log(`Time: ${typeof time}`);
+  return createddate;
+}
 
 function RenderAlerts() {
   const context = useContext(AdminContext);
@@ -67,38 +87,48 @@ function RenderAlerts() {
               <LoadingComponent message="loading" />
             </div>
           ) : (
-            <Tabs>
+            <Card>
               {context.clubs.map(club => (
-                <div label={club.clubname} key={club.clubid}>
-                  <div className="under-tabs-container">
+                <Card>
+                  <Card.Grid style={gridStyle1} hoverable={false}>
+                    {club.clubname}
+                  </Card.Grid>
+                  <div>
                     {sentimentObj[club.clubname]?.map(alert => {
                       return (
-                        <div key={alert.id} className="alertDiv flags box">
-                          <div className="contentDiv">
-                            <h4>Member: {alert.member}</h4>
-                            <h4>Activity: {alert.activities}</h4>
-                            <h4>Time: {alert.createddate}</h4>
-                            <span>
+                        <Tooltip
+                          title={
+                            <Card>
+                              MemberID: {alert.member}
+                              <br />
+                              Activity: {alert.activities}
+                              <br />
+                              Time: {alert.createddate}
+                              <br />
+                              Elapsed Time: {ElapsedTime(alert.createddate)}
+                              <div>
+                                {String.fromCodePoint(
+                                  parseInt(alert.reactionvalue, 16)
+                                )}
+                              </div>
+                            </Card>
+                          }
+                        >
+                          <Card.Grid style={gridStyle2}>
+                            <div>
                               {String.fromCodePoint(
                                 parseInt(alert.reactionvalue, 16)
-                              )}
-                            </span>
-                          </div>
-                          <div className="buttDiv">
-                            <Button
-                              type="primary"
-                              onClick={() => resolveBtn(alert.id)}
-                            >
-                              Resolve
-                            </Button>
-                          </div>
-                        </div>
+                              )}{' '}
+                              {alert.activities}
+                            </div>
+                          </Card.Grid>
+                        </Tooltip>
                       );
                     })}
                   </div>
-                </div>
+                </Card>
               ))}
-            </Tabs>
+            </Card>
           )}
         </Content>
       </Layout>
@@ -106,3 +136,42 @@ function RenderAlerts() {
   );
 }
 export default RenderAlerts;
+
+// {context.clubs.length === 0 ? (
+//   <div className="centered-content flex">
+//     <LoadingComponent message="loading" />
+//   </div>
+// ) : (
+//   <Tabs>
+//     {context.clubs.map(club => (
+//       <div label={club.clubname} key={club.clubid}>
+//         <div className="under-tabs-container">
+//           {sentimentObj[club.clubname]?.map(alert => {
+//             return (
+//               <div key={alert.id} className="alertDiv flags box">
+//                 <div className="contentDiv">
+//                   <h4>Member: {alert.member}</h4>
+//                   <h4>Activity: {alert.activities}</h4>
+//                   <h4>Time: {alert.createddate}</h4>
+//                   <span>
+//                     {String.fromCodePoint(
+//                       parseInt(alert.reactionvalue, 16)
+//                     )}
+//                   </span>
+//                 </div>
+//                 <div className="buttDiv">
+//                   <Button
+//                     type="primary"
+//                     onClick={() => resolveBtn(alert.id)}
+//                   >
+//                     Resolve
+//                   </Button>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     ))}
+//   </Tabs>
+// )}
