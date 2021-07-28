0c3b61c0f8f29ab6c505e06cf4351c95e98e778a
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-07-28 18:28:01 -0400

    drop down menu not complete

--- src/components/pages/ReactionsTable/RenderReactionsTable.js
+++ src/components/pages/ReactionsTable/RenderReactionsTable.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Table, Space } from 'antd';
-import { Layout } from 'antd';
 import NavMenu from '../../common/NavMenu';
 import { LayoutContainer } from '../../common/';
 import NavBar from '../../common/NavBar';
@@ -10,6 +9,8 @@ import { LoadingComponent } from '../../common';
 import './ReactionsTable.css';
 import styled from 'styled-components';
 import { fetchMembersReaction } from '../../../api';
+import { Menu, Dropdown, Layout } from 'antd';
+import { DownOutlined, StockOutlined } from '@ant-design/icons';
 
 const { Content, Sider } = Layout;
 
@@ -65,7 +66,8 @@ const sampleTableData = {
 export default function RenderReactionsTable() {
   const context = useContext(AdminContext);
   const [tableData, setTableData] = useState(sampleTableData);
-
+  const [filterDeata, setFilterData] = useState(sampleTableData);
+  const [whichClub, setWhichClub] = useState('');
   useEffect(() => {
     reactionDataToTableData();
   }, [context]);
@@ -95,6 +97,18 @@ export default function RenderReactionsTable() {
     });
   };
 
+  const menu = (
+    <Menu>
+      {context.clubs.filter(element => (
+        <Menu.Item
+          key={element.id}
+          icon={<StockOutlined />}
+          onClick={() => setWhichClub(element.clubname)}
+          className="menu-club"
+        ></Menu.Item>
+      ))}
+    </Menu>
+  );
   return (
     <LayoutContainer>
       <NavBar titleName={'Manage Reactions'} backgroundColor="#293845" />
@@ -103,6 +117,13 @@ export default function RenderReactionsTable() {
           <NavMenu />
         </Sider>
         <Content>
+          <Dropdown overlay={menu} trigger={['click']}>
+            <a>
+              {' '}
+              className= "ant-dropdown-link" onClick={e => e.preventDefault()}
+            </a>
+            Club Name <DownOutlined />
+          </Dropdown>
           {context.memberReactions.length === 0 ? (
             <div className="centered-content flex">
               <LoadingComponent message="loading" />
